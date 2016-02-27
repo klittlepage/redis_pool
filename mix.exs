b9dfd88e37f091a61efc5e4a5de10d239b70dae9
Kelly Littlepage <kelly.littlepage@gmail.com>
2016-02-26 23:30:37 -0600

    Referenced release versions of exredis and poolboy

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RedisPool.Mixfile do
 
   def project do
     [ app: :redis_pool,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.0",
       deps: deps ]
   end
@@ -16,15 +16,8 @@ defmodule RedisPool.Mixfile do
     ]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
-    [
-      { :eredis, github: "wooga/eredis" },
-      { :poolboy, github: "devinus/poolboy" }
-    ]
+    [{:exredis, "~> 0.2.0"},
+     {:poolboy, "~> 1.5.0"}]
   end
 end
